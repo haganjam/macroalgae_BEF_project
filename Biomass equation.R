7adbffa4c68395890f224853eb92ebf0bbec5d65
brunnerbp <benedikt.p.brunner@gmail.com>
2021-06-07 09:38:18 +0200

    as no calculation

--- Biomass equation.R
+++ Biomass equation.R
@@ -59,3 +59,43 @@ cor(buchanan_dw,allo_dat.fu_se$pred.dryweight)^2
 
 
 
+####Ascophyllum Nodosum#######
+mod1=lm(allo_dat.as_no$dry_weight_g ~ allo_dat.as_no$wet_weight_g)
+summary(mod1)
+
+plot(allo_dat.as_no$wet_weight_g,allo_dat.as_no$dry_weight_g)
+
+
+#Predicting Dryweight
+allo_dat.as_no$pred.dryweight = predict(mod1,newdata = data.frame(allo_dat.as_no$wet_weight_g))
+
+#Cylindrical volume
+allo_dat.as_no$radius = allo_dat.as_no$circum_cm/(2*pi)
+allo_dat.as_no$cyl.vol = allo_dat.as_no$radius^2*pi*allo_dat.as_no$length_cm
+
+
+
+#Creating formula log(ww) ~ log(cyl.vol)
+mod2=lm(log(allo_dat.as_no$wet_weight_g) ~ log(allo_dat.as_no$cyl.vol))
+summary(mod2)
+#plot(mod2) #ok
+
+
+res= data.frame(log.ww=(log(allo_dat.as_no$wet_weight_g)),log.cyl.vol=log(allo_dat.as_no$cyl.vol))
+ggscatter(data=res, y="log.ww",x="log.cyl.vol",add = "reg.line")
+
+#How accurate?
+fitted(mod2)
+pred_as_no=as.data.frame(predict(mod2, newdata = data.frame(log(allo_dat.as_no$cyl.vol)), interval = "confidence"))
+pred_as_no=pred_as_no[order(pred_as_no$fit),]
+pred_as_no=cbind(pred_as_no,name=letters[1:length(pred_as_no$fit)])
+
+pred_as_no$fit=exp(pred_as_no$fit)
+pred_as_no$lwr=exp(pred_as_no$lwr)
+pred_as_no$upr=exp(pred_as_no$upr)
+
+ggplot(pred_as_no) +
+  geom_bar( aes(x=name, y=fit), stat="identity", fill="black", alpha=0.5) +
+  geom_errorbar( aes(x=name, ymin=lwr, ymax=upr), width=0.3, colour="orange", alpha=0.9, size=0.5)
+
+
