5ef515e0ba95233d538ff873fd923bfd641a114f
James Hagan <james_hagan@outlook.com>
2021-06-28 00:02:35 +0200

    interpolate the depth between points

--- transect_depth_analysis.R
+++ transect_depth_analysis.R
@@ -22,73 +22,93 @@ tra_dat <- read_sheet("https://docs.google.com/spreadsheets/d/1H2_RtGusheNz6ibKz
 tra_dat %>%
   View()
 
-df <- 
+# subset the data needed to interpolate the depth to the different points
+depth_data <- 
   tra_dat %>% 
   mutate(depth_correct = (water_level_cm + depth) ) %>%
   select(date, transect_id, position, depth_correct) %>%
-  distinct() %>%
-  filter(transect_id == "1")
+  distinct()
 
-View(df)
+# problems with the start of transect 2, remove positions 0 to 4
+depth_data <- 
+  depth_data %>%
+  filter( !(transect_id == 2 & position %in% 0:4) )
 
-dividers <- which(!is.na(df$depth_correct) )
+# split into a list
+depth_list <- split(depth_data, depth_data$transect_id)
 
-x <- vector("list", length = (length(dividers)-1))
-for(i in 1:(length(dividers)-1) ) {
+# loop over all transects
+depth_out <- vector("list", length = length(depth_list))
+for(i in 1:length(depth_list)) {
   
-  x[[i]] <- rep(i, dividers[i+1] - dividers[i] )
+  # initialise a data.frame to work with
+  df <- depth_list[[i]]
   
-}
-
-x
-
-diff_func <- function(x) {
+  # get the dividers
+  dividers <- which(!is.na(df$depth_correct) )
   
-  x[length(x)] - x[1]
+  # duplicate middles for which the data are needed for multiple calculations
+  if (length(dividers) > 2) {
+    
+    dups <- dividers[-c(1, length(dividers))]
+    
+    df <- 
+      df[c(1:nrow(df), dups), ] %>%
+      arrange(date, transect_id, position)
+    
+  }
   
-}
+  # add an ID column
+  x <- vector("list", length = (length(dividers)-1))
+  for(j in 1:(length(dividers)-1) ) {
+    
+    x[[j]] <- rep(j, ( (dividers[j+1] - dividers[j])+1 ) )
+    
+  }
+  
+  # write the ID column into the data.frame
+  df$position_id <- unlist(x)
+  
+  # split data by the position ID
+  df_list <- split(df, df$position_id)
+  
+  # for each block, interpolate the depth between the points
+  int_depth <- 
+    lapply(df_list, function(y){
+      
+      y %>%
+        group_by(position_id) %>%
+        mutate(y_int = first(depth_correct),
+               d_depth = (last(depth_correct) - first(depth_correct)),
+               d_position = last(10*position) - first(10*position)) %>%
+        ungroup() %>%
+        mutate(slope = d_depth/d_position) %>%
+        select(-d_depth, -d_position) %>%
+        mutate(distance = 0:(length(position)-1)*10 ) %>%
+        mutate(depth_interpolated = y_int + ((distance)*slope) )  %>%
+        select(-distance) %>%
+        mutate(depth_interpolated = if_else(!is.na(depth_correct), depth_correct, depth_interpolated)) %>%
+        select(-y_int, -slope, -position_id)
+      
+    }) %>%
+    bind_rows(.,) %>%
+    distinct()
+  
+  depth_out[[i]] <- int_depth
+  
+} 
 
-depth_exp <- 
-  tra_dat %>% 
-  mutate(depth_correct = (water_level_cm + depth) ) %>%
-  select(date, transect_id, position, depth_correct) %>%
-  distinct() %>%
-  filter(!is.na(depth_correct))
+# bind the list into a data.frame
+depth_out <- bind_rows(depth_out)
 
-View(depth_exp)
-
-# test with the first transect
-y <- 
-  depth_exp %>%
-  filter(transect_id == "1") %>%
-  summarise(position_id = c(zoo::rollapply(data = position, width = 2, FUN = function(y)y[length(y)]  )),
-            m = (zoo::rollapply(data = depth_correct, width = 2, FUN = diff_func))/(zoo::rollapply(data = position, width = 2, FUN = diff_func)*10 ) ,
-            con = zoo::rollapply(data = depth_correct, width = 2, FUN = first))
-
-y
-
-z <- 
-  depth_exp %>%
-  filter(transect_id == "1") %>%
-  mutate(seq_x = slide(position, function(x){seq(x[1], x[length(x)], 1)*10 }, .before = 1, .after = 0, .complete = TRUE  ) ) %>%
-  tidyr::unnest(cols = c("seq_x")) %>%
-  rename(position_id = position)
-
-y
-
-z
-
-full_join(z, y, by = "position_id") %>%
-  mutate(depth_interpolate = m*seq_x + con)
-
-tra_dat %>% 
-  mutate(depth_correct = (water_level_cm + depth) ) %>%
-  select(date, transect_id, position, depth_correct) %>%
-  distinct() %>%
-  filter(transect_id == "1")
+# join this back to the full dataset
+tra_a <- 
+  full_join(tra_dat, depth_out, by = c("date", "transect_id", "position")) %>%
+  select(date, transect_id, site_id, time, position, water_level_cm, 
+         depth, depth_correct, depth_interpolated, length_cm, circum_cm, field_obs, note)
+  
+# work with the transect data
 
 
 
-abs(zoo::rollapply(data = x, width = 2, FUN = diff))
 
-abs(zoo::rollapply(data = position, width = 2, FUN = diff))
