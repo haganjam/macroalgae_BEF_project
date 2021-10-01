b371febdfe5e4222c3ac233ea56e04e8c93f8bf3
James Hagan <james_hagan@outlook.com>
2021-10-01 10:40:14 +0200

    update gitignore and add a script to download the logger data from the tile experiment from the google drive

---
+++ download_tile_experiment_logger_data.R
@@ -0,0 +1,28 @@
+
+# Project: Tile experiment
+
+# Title: Download the logger data from the google drive
+
+# load relevant libraries
+library(googledrive)
+library(here)
+
+# make a folder to download the tile data
+if(! dir.exists(here("tile_logger_data"))){
+  dir.create(here("tile_logger_data"))
+}
+
+# get a list of the logger data files in the Google Drive
+file_id <- drive_ls(path = "hobo_logger_tiles")
+
+# subset the csv files
+file_names <- file_id$name[grepl(pattern = ".csv", file_id$name) ]
+
+# write a loop to download the files onto the local machine
+for(i in 1:length(file_names)) {
+  
+  drive_download(file = file_names[i], 
+                 path = paste(here("tile_logger_data"),"/", file_names[i], sep = "") )
+  
+}
+
