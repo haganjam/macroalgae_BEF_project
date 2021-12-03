5d07a8d50fb5acb50bcd8cf3b180f1ec37d1a8c1
James Hagan <james_hagan@outlook.com>
2021-12-03 18:07:22 +0100

    remove the google sheet analysis and use the raw csv file, load the plotting theme and make the depth distribution of the four species for the Fig. 1

--- transect_analysis/transect_depth_analysis.R
+++ transect_analysis/transect_depth_analysis.R
@@ -1,27 +1,22 @@
 
 # Project: Functional value of macroalgal biodiversity
 
-# Title: Species x depth gradient
-
-# load relevant libraries
-library(googlesheets4)
-library(dplyr)
-library(here)
-library(slider)
+# Title: Preliminary analysis of depth data to pick the experimental depths
 
 # load libraries using groundhog
 library(groundhog)
 groundhog.day <- "2020-06-1"
-pkgs <- c("here", "dplyr", "readr", "googlesheets4", "ggplot2", "slider")
+pkgs <- c("here", "dplyr", "readr", "ggplot2", "slider",
+          "ggforce", "gghalves", "ggbeeswarm")
 groundhog.library(pkgs, groundhog.day)
 
+# load relevant functions
+source(here("functions/function_plotting_theme.R"))
 
-# import the transect data
-
-# import the raw data from the Google Sheet
-tra_dat <- read_sheet("https://docs.google.com/spreadsheets/d/1H2_RtGusheNz6ibKzoCqLrEAYURznQuDS1bPCPnF_SA/edit#gid=0", sheet = "transect_samples",
-                       col_types = c("ccccccccnnncnncc"),
-                       na = c("NA"))
+# import the raw transect data
+tra_dat <- read_csv(file = here("preliminary_supporting_data/transect_data.csv"),
+                    col_types = c("ccccccccnnncnncc"),
+                    na = c("NA"))
 names(tra_dat)
 
 # subset the data needed to interpolate the depth to the different points
@@ -32,10 +27,10 @@ depth_data <-
   select(date, transect_id, position, depth_correct) %>%
   distinct()
 
-View(depth_data)
+# View(depth_data)
 
 # problems with the start of transect 2, remove positions 0 to 4
-# we do not have a starting depth...
+# we do not have a starting depth
 depth_data <- 
   depth_data %>%
   filter( !(transect_id == 2 & position %in% 0:4) )
@@ -106,6 +101,7 @@ for(i in 1:length(depth_list)) {
 
 # bind the list into a data.frame
 depth_out <- bind_rows(depth_out)
+# View(depth_out)
 
 # join this back to the full dataset
 tra_a <- 
@@ -113,17 +109,26 @@ tra_a <-
   select(date, transect_id, site_code, time, position, water_level_cm, 
          depth_cm, depth_correct, depth_interpolated, binomial_code, length_cm, circum_cm, 
          field_observer, notes)
+# View(tra_a)
 
-View(tra_a)
+# check the summary statistics
+summary(tra_a)
+
+# check for unique values, especially for the binomial codes
+lapply(tra_a, function(x) unique(x))
+
+# remove missing binomial codes and the missing values "", NA
+tra_a <- 
+  tra_a %>%
+  filter( !(binomial_code %in% c("-9999", "") | is.na(binomial_code) | is.na(depth_interpolated) )  )
   
 # work with the transect data
-ggplot(data = tra_a %>% filter(binomial_code != -9999),
+ggplot(data = tra_a,
        mapping = aes(x = binomial_code, y = depth_interpolated)) +
   geom_point()
 
 transect_summary <- 
   tra_a %>% 
-  filter(binomial_code != -9999) %>%
   group_by(binomial_code) %>%
   summarise(mean_depth = mean(depth_interpolated, na.rm = TRUE),
             min_depth = min(depth_interpolated, na.rm = TRUE),
@@ -137,17 +142,24 @@ transect_summary <-
   transect_summary %>%
   select(data_id, binomial_code, mean_depth:quant_80)
 
+# View(transect_summary)
 
-# import raw allometric data from the Google Sheet
-allo_dat <- read_sheet("https://docs.google.com/spreadsheets/d/167zCNjbmZ1PV5V1vZeGe9QcIrvhJ4diZ8q2rF9Ggry0/edit#gid=0", sheet = "Sheet1",
-                       col_types = c("ccccdcccccdcddddccdddccdccc"),
-                       na = c("NA"))
+
+# load the raw allometric data
+allo_dat <- read_csv(file = here("preliminary_supporting_data/sample_data_biomass_allometry.csv"),
+                     col_types = c("ccccdcccccdcddddccdddccdccc"),
+                     na = c("NA"))
+
+# remove the missing values from allo_dat
+allo_dat <- 
+  allo_dat %>%
+  filter(!is.na(depth_cm)) %>%
+  filter(!is.na(water_level_cm)) %>%
+  mutate(depth_correct = depth_cm + water_level_cm)
+  
 
 # plot out the depth distribution
 allo_dat %>%
-  filter(!is.na(depth_cm)) %>%
-  filter(!is.na(water_level_cm)) %>%
-  mutate(depth_correct = depth_cm + water_level_cm) %>%
   ggplot(data = ., 
          mapping = aes(x = binomial_code, y = depth_correct)) +
   geom_point()
@@ -155,9 +167,6 @@ allo_dat %>%
 # get summary statistics
 allo_summary <- 
   allo_dat %>%
-  filter(!is.na(depth_cm)) %>%
-  filter(!is.na(water_level_cm)) %>%
-  mutate(depth_correct = depth_cm + water_level_cm) %>%
   group_by(binomial_code) %>%
   summarise(mean_depth = mean(depth_correct, na.rm = TRUE),
             min_depth = min(depth_correct, na.rm = TRUE),
@@ -165,12 +174,9 @@ allo_summary <-
             quant_20 = quantile(depth_correct, 0.20, na.rm = TRUE),
             quant_80 = quantile(depth_correct, 0.80, na.rm = TRUE))
 
-allo_dat %>%
-  filter(!is.na(depth_cm)) %>%
-  filter(!is.na(water_level_cm)) %>%
-  mutate(depth_correct = depth_cm + water_level_cm) %>%
-  filter(depth_correct > 10) %>%
-  View()
+# allo_dat %>%
+  # filter(depth_correct > 10) %>%
+  # View()
 
 allo_summary$data_id <- "allometry_data"
 
@@ -178,11 +184,63 @@ allo_summary <-
   allo_summary %>%
   select(data_id, binomial_code, mean_depth:quant_80)
 
-bind_rows(transect_summary, allo_summary) %>%
-  write_csv(x = ., file = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_2_Fucus_landscape/datasheets_print/zonation_data.csv")
+# View(allo_summary)
+
+# bind_rows(transect_summary, allo_summary) %>%
+  # write_csv(x = ., file = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_2_Fucus_landscape/datasheets_print/zonation_data.csv")
 
 
+# plot the depth data for each species
+names(allo_dat)
+names(tra_a)
+
+# bind these data together
+all_depth <- 
+  bind_rows(
+    
+    select(allo_dat, binomial_code, depth_correct ) %>%
+      mutate(data_set = "allometry"),
+    
+    select(tra_a, binomial_code, depth_correct = depth_interpolated) %>%
+      mutate(data_set = "transect")
+    
+  )
 
 
+# change the order of the binomial codes
+all_depth$binomial_code <- factor(all_depth$binomial_code, levels = c("fu_se", "as_no", "fu_ve", "fu_sp" ))
+levels(all_depth$binomial_code) = c("F. serratus", "A. nodosum", "F. vesiculosus", "F. spiralis")
 
+all_depth_summary <- 
+  all_depth %>%
+  group_by(binomial_code) %>%
+  summarise(m_depth_correct = mean(depth_correct), 
+            sd_depth_correct = sd(depth_correct), .groups = "drop") %>%
+  mutate(upper = (m_depth_correct + sd_depth_correct),
+         lower = (m_depth_correct - sd_depth_correct))
 
+ggplot() +
+  geom_errorbarh(data = all_depth_summary,
+                mapping = aes(xmin = lower,
+                              xmax = upper,
+                              y = binomial_code, colour = binomial_code),
+                height = 0.1) +
+  geom_quasirandom(data = all_depth,
+                   mapping = aes(x = depth_correct, y = binomial_code, colour = binomial_code),
+                   groupOnX = FALSE, alpha = 0.3) +
+  geom_point(data = all_depth_summary, 
+             mapping = aes(x = m_depth_correct,
+                           y = binomial_code, colour = binomial_code), size = 2) +
+  geom_point(mapping = aes(x = sort(c(-5, -12, -28, -40)), y = rep(0.1, 4),
+                           colour = all_depth_summary$binomial_code),
+             shape = 73, size = 9) +
+  scale_x_continuous(limits = c(-55, 18), 
+                     breaks = seq(-55, 18, 10)) +
+  scale_colour_viridis_d(option = "C") +
+  theme_meta() +
+  ylab(NULL) +
+  xlab("Depth (cm)") +
+  theme(legend.position = "none")
+
+### END  
+  
