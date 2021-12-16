4454c10bd30b52b920c6b55a437428fc81963397
James Hagan <james_hagan@outlook.com>
2021-12-16 17:24:49 +0200

    split the species depth analysis into cleaning and analyses

---
+++ analysis_scripts/species_depth_analysis.R
@@ -0,0 +1,67 @@
+
+# Project: Functional value of macroalgal biodiversity
+
+# Title: Analyse the depth data from the transects and allometry data collections
+
+# load libraries using groundhog
+library(groundhog)
+groundhog.day <- "2020-06-1"
+pkgs <- c("here", "dplyr", "readr", "ggplot2", "slider",
+          "ggforce", "gghalves", "ggbeeswarm")
+groundhog.library(pkgs, groundhog.day)
+
+# load relevant functions
+source(here("functions/function_plotting_theme.R"))
+
+# load in the cleaned species depth data
+all_depth <- read_csv(file = here("analysis_data/species_depth_data.csv"))
+
+# change the order of the binomial codes
+all_depth$binomial_code <- factor(all_depth$binomial_code, levels = c("fu_se", "as_no", "fu_ve", "fu_sp" ))
+levels(all_depth$binomial_code) = c("F. serratus", "A. nodosum", "F. vesiculosus", "F. spiralis")
+
+all_depth_summary <- 
+  all_depth %>%
+  group_by(binomial_code) %>%
+  summarise(m_depth_correct = mean(depth_correct), 
+            sd_depth_correct = sd(depth_correct), .groups = "drop") %>%
+  mutate(upper = (m_depth_correct + sd_depth_correct),
+         lower = (m_depth_correct - sd_depth_correct))
+
+p1 <- 
+  ggplot() +
+  geom_errorbarh(data = all_depth_summary,
+                 mapping = aes(xmin = lower,
+                               xmax = upper,
+                               y = binomial_code, colour = binomial_code),
+                 height = 0.1) +
+  geom_quasirandom(data = all_depth,
+                   mapping = aes(x = depth_correct, y = binomial_code, colour = binomial_code),
+                   groupOnX = FALSE, alpha = 0.2, shape = 16) +
+  geom_point(data = all_depth_summary, 
+             mapping = aes(x = m_depth_correct,
+                           y = binomial_code, colour = binomial_code), size = 2) +
+  geom_point(mapping = aes(x = sort(c(-5, -12, -28, -40)), y = rep(0.1, 4),
+                           colour = all_depth_summary$binomial_code),
+             shape = 73, size = 9) +
+  scale_x_continuous(limits = c(-55, 18), 
+                     breaks = seq(-55, 18, 10)) +
+  scale_y_discrete() +
+  scale_colour_viridis_d(option = "C") +
+  theme_meta() +
+  ylab(NULL) +
+  xlab("Depth (cm)") +
+  theme(legend.position = "none",
+        axis.text.y = element_text(hjust = 0.5, size = 9),
+        axis.text.x = element_text(size = 9),
+        axis.title.x = element_text(size = 10.5))
+
+# make a folder to export the cleaned data
+if(! dir.exists(here("figures"))){
+  dir.create(here("figures"))
+}
+
+ggsave(filename = here("figures/fig_1.png"), p1, units = "cm", dpi = 450,
+       width = 8, height = 6)
+
+### END  
--- cleaning_scripts/5_process_species_depth_data.R
+++ cleaning_scripts/5_process_species_depth_data.R
@@ -1,17 +1,13 @@
 
 # Project: Functional value of macroalgal biodiversity
 
-# Title: Preliminary analysis of depth data to pick the experimental depths
+# Title: Clean and process the transect and allometry depth data
 
 # load libraries using groundhog
 library(groundhog)
 groundhog.day <- "2020-06-1"
-pkgs <- c("here", "dplyr", "readr", "ggplot2", "slider",
-          "ggforce", "gghalves", "ggbeeswarm")
+pkgs <- c("here", "dplyr", "readr", "ggplot2")
 groundhog.library(pkgs, groundhog.day)
-
-# load relevant functions
-source(here("functions/function_plotting_theme.R"))
 
 # import the raw transect data
 tra_dat <- read_csv(file = here("preliminary_supporting_data/transect_data.csv"),
@@ -184,11 +180,12 @@ allo_summary <-
   allo_summary %>%
   select(data_id, binomial_code, mean_depth:quant_80)
 
+# we used these summary statistics to choose our experimental depths
+
 # View(allo_summary)
 
 # bind_rows(transect_summary, allo_summary) %>%
   # write_csv(x = ., file = "C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_2_Fucus_landscape/datasheets_print/zonation_data.csv")
-
 
 # plot the depth data for each species
 names(allo_dat)
@@ -207,53 +204,7 @@ all_depth <-
   )
 
 
-# change the order of the binomial codes
-all_depth$binomial_code <- factor(all_depth$binomial_code, levels = c("fu_se", "as_no", "fu_ve", "fu_sp" ))
-levels(all_depth$binomial_code) = c("F. serratus", "A. nodosum", "F. vesiculosus", "F. spiralis")
+# write this into a .csv file into the analysis data
+write_csv(x = all_depth, path = here("analysis_data/species_depth_analysis.csv"))
 
-all_depth_summary <- 
-  all_depth %>%
-  group_by(binomial_code) %>%
-  summarise(m_depth_correct = mean(depth_correct), 
-            sd_depth_correct = sd(depth_correct), .groups = "drop") %>%
-  mutate(upper = (m_depth_correct + sd_depth_correct),
-         lower = (m_depth_correct - sd_depth_correct))
-
-p1 <- 
-  ggplot() +
-  geom_errorbarh(data = all_depth_summary,
-                mapping = aes(xmin = lower,
-                              xmax = upper,
-                              y = binomial_code, colour = binomial_code),
-                height = 0.1) +
-  geom_quasirandom(data = all_depth,
-                   mapping = aes(x = depth_correct, y = binomial_code, colour = binomial_code),
-                   groupOnX = FALSE, alpha = 0.2, shape = 16) +
-  geom_point(data = all_depth_summary, 
-             mapping = aes(x = m_depth_correct,
-                           y = binomial_code, colour = binomial_code), size = 2) +
-  geom_point(mapping = aes(x = sort(c(-5, -12, -28, -40)), y = rep(0.1, 4),
-                           colour = all_depth_summary$binomial_code),
-             shape = 73, size = 9) +
-  scale_x_continuous(limits = c(-55, 18), 
-                     breaks = seq(-55, 18, 10)) +
-  scale_y_discrete() +
-  scale_colour_viridis_d(option = "C") +
-  theme_meta() +
-  ylab(NULL) +
-  xlab("Depth (cm)") +
-  theme(legend.position = "none",
-        axis.text.y = element_text(hjust = 0.5, size = 9),
-        axis.text.x = element_text(size = 9),
-        axis.title.x = element_text(size = 10.5))
-
-# make a folder to export the cleaned data
-if(! dir.exists(here("figures"))){
-  dir.create(here("figures"))
-}
-
-ggsave(filename = here("figures/fig_1.png"), p1, units = "cm", dpi = 450,
-       width = 8, height = 6)
-
-### END  
-  
+### END
