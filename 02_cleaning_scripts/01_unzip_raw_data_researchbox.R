aba43ecf276751403c800dc2d20c82b3b6ac3aa6
James Hagan <james_hagan@outlook.com>
2022-07-20 12:25:38 +0200

    add a script to unzip the researchbox data and then rename the cleaning scripts

---
+++ 02_cleaning_scripts/01_unzip_raw_data_researchbox.R
@@ -0,0 +1,24 @@
+#'
+#' @title: Unzip the ResearchBox data and save it in the current directory
+#' 
+#' @description: This script unzips the ResearchBox fie downloaded from 
+#' the our ResearchBox that stores all the raw data from the project. The raw data 
+#' are stored at the following URL: https://researchbox.org/435&PEER_REVIEW_passcode=ECOTGX.
+#' You should go to the research box and click the button: "Download Entire Box". The
+#' file that is downloaded is a .zip file. This should be stored in your current
+#' working directory.
+#' 
+#' @authors: James G. Hagan (james_hagan(at)outlook.com)
+#' 
+
+# load relevant libraries
+require(here)
+
+# unzip the file
+unzip(zipfile = here("ResearchBox_435.zip"),
+      exdir = here())
+
+# remove the zip file
+unlink(here("ResearchBox_435.zip"))
+
+### END
